c55ff1dfcc222c3cf05aee809a07878047914be3
Maic Lopez Saenz <maic@lopsae.com>
2016-01-15 16:20:12 -0800

    adding rest of constructors for UIBarButtonItem

--- Pod/Extensions/UIBarButtonItem.swift
+++ Pod/Extensions/UIBarButtonItem.swift
@@ -7,9 +7,54 @@ import UIKit
 
 public extension UIBarButtonItem {
 
-	public convenience init(title: String, style: UIBarButtonItemStyle, action: VoidClosure) {
+	public convenience init(title: String,
+		style: UIBarButtonItemStyle,
+		action: VoidClosure
+	) {
 		let closure = ClosureSelector(closure: action)
 		self.init(title: title,
+			style: style,
+			target: closure,
+			action: closure.selector()
+		)
+		WeakKeeper.keep(closure, owner: self)
+	}
+
+
+	convenience init(barButtonSystemItem: UIBarButtonSystemItem,
+		action: VoidClosure
+	) {
+		let closure = ClosureSelector(closure: action)
+		self.init(barButtonSystemItem: barButtonSystemItem,
+			target: closure,
+			action: closure.selector()
+		)
+		WeakKeeper.keep(closure, owner: self)
+	}
+
+
+	convenience init(image: UIImage?,
+		style: UIBarButtonItemStyle,
+		action: VoidClosure
+	) {
+		let closure = ClosureSelector(closure: action)
+		self.init(image: image,
+			style: style,
+			target: closure,
+			action: closure.selector()
+		)
+		WeakKeeper.keep(closure, owner: self)
+	}
+
+
+	convenience init(image: UIImage?,
+		landscapeImagePhone: UIImage?,
+		style: UIBarButtonItemStyle,
+		action: VoidClosure
+	) {
+		let closure = ClosureSelector(closure: action)
+		self.init(image: image,
+			landscapeImagePhone: landscapeImagePhone,
 			style: style,
 			target: closure,
 			action: closure.selector()
